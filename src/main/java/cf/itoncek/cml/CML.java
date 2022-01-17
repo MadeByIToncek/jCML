28e18981f09a4ca4f863db18f272e73914928d2a
MadeByIToncek <47974277+MadeByIToncek@users.noreply.github.com>
2022-01-17 18:55:09 +0100

    Registering commands after startup

--- src/main/java/cf/itoncek/cml/CML.java
+++ src/main/java/cf/itoncek/cml/CML.java
@@ -6,6 +6,7 @@ import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.command.ApplicationCommandInteractionOption;
 import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
@@ -17,6 +18,7 @@ import discord4j.core.object.entity.Message;
 import discord4j.discordjson.json.MessageCreateRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,12 +32,19 @@ public class CML {
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
-        try {
-            new GlobalCommandRegistrar(client).registerCommands();
-        } catch (Exception e) {
-            logger.error(e.toString());
-            logger.error(e.getMessage());
-        }
+
+        gateway.on(ReadyEvent.class, event ->
+                Mono.fromRunnable(() -> {
+                    try {
+                        new GlobalCommandRegistrar(client).registerCommands();
+                    } catch (Exception e) {
+                        logger.error(e.toString());
+                        logger.error(e.getMessage());
+                    }
+                    System.out.printf("Logged in as %s#%s%n", gateway.getSelf().block().getUsername(), gateway.getSelf().block().getDiscriminator());
+                })
+        );
+
 
         gateway.on(MessageCreateEvent.class).subscribe(event -> {
             final Message message = event.getMessage();
@@ -47,7 +56,7 @@ public class CML {
 
 
         gateway.on(ChatInputInteractionEvent.class, event -> {
-            switch(event.getCommandName()){
+            switch (event.getCommandName()) {
                 case "ping":
                     return event.reply("Beep boop, I'm awake").withEphemeral(true);
                 case "jsem":
@@ -56,31 +65,31 @@ public class CML {
                             .map(ApplicationCommandInteractionOptionValue::asString)
                             .orElse("null");
                     List<ActionComponent> actionComponentList = new ArrayList<>();
-                    Button yes = Button.success("xyz"+event.getInteraction().getUser().getId().asLong() + "." + id , "Povolit");
-                    Button no = Button.danger("rjct" , "Odmítnout");
+                    Button yes = Button.success("xyz" + event.getInteraction().getUser().getId().asLong() + "." + id, "Povolit");
+                    Button no = Button.danger("rjct", "Odmítnout");
                     actionComponentList.add(yes);
                     actionComponentList.add(no);
                     ActionRow ac = ActionRow.of(actionComponentList);
                     MessageCreateRequest msg = MessageCreateRequest.builder().content(event.getInteraction().getUser().getMention() + " požádal o roli <@&" + id + ">").addComponent(ac.getData()).build();
                     client.getChannelById(Snowflake.of(931914067331923968L)).createMessage(msg).block();
-                    return event.reply("Žádost o roli <@&"+id+"> byla odeslána").withEphemeral(true);
+                    return event.reply("Žádost o roli <@&" + id + "> byla odeslána").withEphemeral(true);
             }
             return null;
         }).subscribe();
 
         gateway.on(ButtonInteractionEvent.class, event -> {
             String id = event.getCustomId();
-            if (id.startsWith("rjc")){
+            if (id.startsWith("rjc")) {
                 event.getMessage().get().delete("Denied");
                 return event.reply("Uživatel byl odmítnut").withEphemeral(true);
-            } else if (id.startsWith("xyz")){
+            } else if (id.startsWith("xyz")) {
                 id = id.substring(3);
                 String userID = id.split("[.]")[0];
                 String roleID = id.split("[.]")[1];
                 Guild guild = event.getInteraction().getGuild().block();
                 event.getMessage().get().delete("Allowed").block();
                 guild.getMemberById(Snowflake.of(userID)).block().addRole(Snowflake.of(roleID)).block();
-                return event.reply(gateway.getUserById(Snowflake.of(userID)).block().getUsername() + " byl povolen přístup k kanálům tábora '"+ event.getInteraction().getGuild().block().getRoleById(Snowflake.of(roleID)).block().getName()+"'");
+                return event.reply(gateway.getUserById(Snowflake.of(userID)).block().getUsername() + " byl povolen přístup k kanálům tábora '" + event.getInteraction().getGuild().block().getRoleById(Snowflake.of(roleID)).block().getName() + "'");
             }
             return event.reply("test").withEphemeral(true);
         }).subscribe();
